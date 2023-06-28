4a55873903cdae709d36c4d3d9619e1c6dcfd052
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-06-28 11:57:02 +0300

    add Update Contact

--- src/client/app.js
+++ src/client/app.js
@@ -2,7 +2,6 @@
 function loadContacts() {
   axios.get('http://localhost:8000/contacts/')
     .then(response => {
-
       const contacts = response.data;
       // Очищення таблиці
       const tableBody = document.getElementById('contactTableBody');
@@ -19,11 +18,12 @@ function loadContacts() {
           <td>${contact.Contact.birthday}</td>
           <td>${contact.Contact.phone_number}</td>
           <td>${contact.Contact.additional_data ?? ""}</td>
+          <td><button onclick="showUpdateModal(${contact.Contact.id})">Update</button></td>
           <td><button onclick="deleteContact(${contact.Contact.id})">Delete</button></td>
         `;
         tableBody.appendChild(row);
       });
-      
+
     })
     .catch(error => {
       console.error('Failed to load contacts:', error);
@@ -54,10 +54,8 @@ async function createContact(event) {
     const response = await axios.post('http://localhost:8000/contacts/', newContact);
     console.log(response);
 
-  
     createContactForm.reset();
     createContactModal.hide();
-
 
     // Завантаження оновленого списку контактів
     loadContacts();
@@ -70,9 +68,9 @@ const createContactForm = document.getElementById('createContactForm');
 createContactForm.addEventListener('submit', createContact);
 const createContactModal = new bootstrap.Modal(document.getElementById('createContactModal'));
 
-
-
-
+const updateContactForm = document.getElementById('updateContactForm');
+updateContactForm.addEventListener('submit', showUpdateModal);
+const updateContactModal = new bootstrap.Modal(document.getElementById('updateContactModal'));
 
 // Функція для видалення контакту
 async function deleteContact(contactId) {
@@ -92,13 +90,64 @@ async function deleteContact(contactId) {
   }
 }
 
-
 // Функція для редагування контакту
-async function editContact(contactId) {
+async function updateContact(contactId) {
+  try {
+    const response = await axios.get(`http://localhost:8000/contacts/${contactId}`);
+    const contact = response.data;
 
+    const firstNameInput = document.getElementById('updateFirstNameInput');
+    const lastNameInput = document.getElementById('updateLastNameInput');
+    const emailInput = document.getElementById('updateEmailInput');
+    const phoneNumberInput = document.getElementById('updatePhoneNumberInput');
+    const birthdayInput = document.getElementById('updateBirthdayInput');
+    const additionalDataInput = document.getElementById('updateAdditionalDataInput');
+
+    firstNameInput.value = contact.first_name;
+    lastNameInput.value = contact.last_name;
+    emailInput.value = contact.email;
+    phoneNumberInput.value = contact.phone_number;
+    birthdayInput.value = contact.birthday;
+    additionalDataInput.value = contact.additional_data ?? "";
+
+    updateContactForm.onsubmit = async (event) => {
+      event.preventDefault();
+
+      const updatedContact = {
+        first_name: firstNameInput.value,
+        last_name: lastNameInput.value,
+        email: emailInput.value,
+        phone_number: phoneNumberInput.value,
+        birthday: birthdayInput.value,
+        additional_data: additionalDataInput.value,
+      };
+
+      try {
+        const response = await axios.put(`http://localhost:8000/contacts/${contactId}`, updatedContact);
+        console.log(response);
+
+        updateContactForm.reset();
+        updateContactModal.hide();
+
+        // Завантаження оновленого списку контактів
+        loadContacts();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    updateContactModal.show();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// завантаження контактів під час завантаження сторінки
+function showUpdateModal(contactId) {
+  updateContact(contactId);
+  updateContactModal.show();
+}
+
+// Завантаження контактів під час завантаження сторінки
 window.addEventListener('load', () => {
   loadContacts();
 });
